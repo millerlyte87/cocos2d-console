7ff47b4e9c18c1f3ec1fcba81831d050e8feb5d7
millerlyte87 <mmiller@Matthews-MacBook-Pro.local>
2018-04-29 12:55:59 -0700

    Updated apk output folder

--- plugins/plugin_compile/build_android.py
+++ plugins/plugin_compile/build_android.py
@@ -604,7 +604,7 @@ class AndroidBuilder(object):
             if project_name is None:
                 # use default project name
                 project_name = 'app'
-            gen_apk_folder = os.path.join(self.app_android_root, 'app/build/outputs/apk')
+            gen_apk_folder = os.path.join(self.app_android_root, 'app/build/outputs/apk/release')
         else:
             assets_dir = os.path.join(self.app_android_root, "assets")
             project_name = self._xml_attr(self.app_android_root, 'build.xml', 'project', 'name')
